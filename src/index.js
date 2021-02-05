aee185c8ff730d8a29143e6c4a2ecea1968bd1b2
sanfernoronha <sanfernoronha99@gmail.com>
2021-02-05 13:13:14 +0530

    :sparkles: added redux and initial store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import "./index.css";
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//store imports
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
+
+//store
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
