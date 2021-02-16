2cfc3b098c6bc97050156457008ae91a27da12d1
sanfernoronha <sanfernoronha99@gmail.com>
2021-02-16 12:39:38 +0530

    :art: added axios config file base url and timeout

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from "../constants/actionType";
 import axios from "axios";
+import API from "../components/global/axios";
 //example init state
 
 const initState = {
@@ -15,14 +16,15 @@ const rootReducer = (state = initState, action) => {
     console.log(name);
     console.log(password);
 
-    axios({
-      method: "POST",
-      url: "http://localhost:5000/customer/signin",
-      data: {
-        name: name,
-        password: password,
-      },
-    })
+    // axios({
+    //   method: "POST",
+    //   url: "http://localhost:5000/customer/signin",
+    //   data: {
+    //     name: name,
+    //     password: password,
+    //   },
+    // })
+    API.post("/customer/signin", { name: name, password: password })
       .then((response) => {
         console.log(response);
         localStorage.setItem("authToken", response.data.accessToken);
