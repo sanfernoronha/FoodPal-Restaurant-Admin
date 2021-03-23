29e78c21e63b659e363e7a7f13d1d85a5023b71e
sanfernoronha <sanfernoronha99@gmail.com>
2021-03-23 09:18:22 +0530

    modified dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -163,17 +163,16 @@ function Dashboard({ actions, state }) {
         <div className={classes.appBarSpacer} />
         <Container maxWidth='lg' className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}></Paper>
             </Grid>
-            {/* Recent Deposits */}
+
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}>
                 <Deposits />
               </Paper>
             </Grid>
-            {/* Recent Orders */}
+
             <Grid item xs={12}>
               <Paper className={classes.paper}>
                 <Orders />
